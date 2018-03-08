2a5c653cdca2e134115ccc5f24e4202af5772de0
Norbert Muller <mullernorbert93@gmail.com>
2018-03-08 21:08:04 +0100

    prototype

---
+++ sqlgen.js
@@ -0,0 +1,41 @@
+/* function makeid () {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < 12; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+} 
+let nameGen = makeid();
+console.log(nameGen);*/
+
+let name = ['Bed', 'Chair', 'Table', 'Vase', 'Computer', 'TV', 'Armchair', 'Speaker', 'Picture', 'Gardrobe', 'Carpet'];
+let purpose = ['to eat', 'to sit', 'to lie', 'to drink', 'to rest'];
+let dimnesions = ['30x40', '50x50', '80x20', '10x10', '150x120', '180x180', '90x90'];
+let color = ['Brown', 'Red', 'Black', 'White', 'Green', 'Yellow', 'Blue'];
+
+const nameGen = () => {
+  return name[Math.floor(Math.random() * name.length)];
+};
+
+const purposeGen = () => {
+  return purpose[Math.floor(Math.random() * purpose.length)];
+};
+
+const dimnesionsGen = () => {
+  return dimnesions[Math.floor(Math.random() * dimnesions.length)];
+};
+const colorGen = () => {
+  return color[Math.floor(Math.random() * color.length)];
+};
+
+function getRandomInt (max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+let id = 0;
+for (let i = 0; i < 622; i++) {
+  id++;
+  console.log("insert into furnitures (id, name, purpose, dimnesions, room, color, has_foot, num_of_legs) values (" + id + ", '" + nameGen() + "', '" + purposeGen() + "', '" + dimnesionsGen() + "', + " + getRandomInt(5) + " + '" + colorGen() + "', 0, " + getRandomInt(4));
+}
+
